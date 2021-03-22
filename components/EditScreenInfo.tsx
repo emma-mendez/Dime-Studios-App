7652e68d469ff1134c1e6145a86c7f056a6fac15
Emma Mendez <emmamendez07@gmail.com>
2021-03-22 00:12:32 +0000

    working code and clean site change to link name

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -18,7 +18,7 @@ export default function EditScreenInfo({ }: { path: string }) {
         <Text
           style={styles.getStartedText} lightColor={Colors.light.tint}
           darkColor="rgba(255,255,255,0.8)">    
-          Members Log In
+          VIP Prices Click Here
         </Text>
         </TouchableOpacity>
 
