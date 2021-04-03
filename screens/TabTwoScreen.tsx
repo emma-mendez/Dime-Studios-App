20a45042041c3b51b03608665edc323f803d151c
Emma Mendez <emmamendez07@gmail.com>
2021-04-03 23:36:37 +0100

    small change to merge onto main attempt 1

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Image, StyleSheet } from 'react-native';
-
+import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabTwoScreen() {
@@ -10,7 +10,7 @@ export default function TabTwoScreen() {
         style={styles.logo}
         source={require("../assets/dimelogo.jpg")} />      
     <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      
+    <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
