97bd856630c5f67ada6550f49782495579ea08a0
Emma Mendez <emmamendez07@gmail.com>
2021-04-05 22:14:02 +0100

    working code

---
+++ screens/data.tsx
@@ -0,0 +1,36 @@
+export default [
+  {
+    type: 'Dance ',
+    imageUri: require('../assets/dimeinside.jpg'),
+    heading: 'Exercise & Move',
+    description: 'For The Choreographers, Dancers and Excercise Fans',
+    key: 'first',
+    // color: '#9dcdfa',
+  },
+  {
+    type: 'Inspirational',
+    imageUri: require('../assets/dimeinside.jpg'),
+    heading: ' Photography',
+    description: 'All Creatives Feel At Home',
+    key: 'second',
+    // color: '#db9efa',
+  },
+  {
+    type: 'Musical',
+    imageUri: require('../assets/dimeinside.jpg'),
+    heading: ' Band Rehearsal',
+    description:
+      'Mics, Leads, Backline for Live and Karaoke',
+    key: 'third',
+    // color: '#999',
+  },
+  {
+    type: 'Environment',
+    imageUri: require('../assets/dimeinside.jpg'),
+    heading: 'Recording',
+    description:
+      'Record your music, podcasts, produce, engineer create a whole thang!!',
+    key: 'fourth',
+    // color: '#a1e3a1',
+  },
+];
